474258a7da97bf8fc786f8ef08a3b3d96caf9460
Vagner do Carmo <vluzrmos@gmail.com>
2021-12-14 18:47:58 -0300

    add php/1016

---
+++ php/1016.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+$X = 60;
+$Y = 90;
+
+$distance = intval(fgets(STDIN));
+
+$minutos = $distance * 2;
+
+echo sprintf('%d minutos', $minutos).PHP_EOL;
